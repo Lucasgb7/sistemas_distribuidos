69162a503e67ee07cbef2aaf09f2efdc1c4d067a
Lucas <lucasgb7_ks@hotmail.com>
2021-10-19 10:10:49 -0300

    tentando conectar url do professor

--- RabbitMQ/example_consumer.py
+++ RabbitMQ/example_consumer.py
@@ -1,25 +1,29 @@
 # example_consumer.py
 import pika, os, time
-58
+
 def pdf_process_function(msg):
- print(" PDF processing")
- print(" [x] Received " + str(msg))
- time.sleep(5) # delays for 5 seconds
- print(" PDF processing finished");
- return;
+    print(" PDF processing")
+    print(" [x] Received " + str(msg))
+    time.sleep(5) # delays for 5 seconds
+    print(" PDF processing finished");
+    return;
+
 # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
-url = os.environ.get('CLOUDAMQP_URL', 'amqps://yixrfvrp:Dqo7-CU2AmPrmkVhr1fqk2g6h7HMF2c5@jackal.rmq.cloudamqp.com/yixrfvrp')
+#url = os.environ.get('CLOUDAMQP_URL', 'amqps://yixrfvrp:Dqo7-CU2AmPrmkVhr1fqk2g6h7HMF2c5@jackal.rmq.cloudamqp.com/yixrfvrp')
+url = os.environ.get('CLOUDAMQP_URL','amqps://tqzvyxix:oGh4wcsnvBrG2U5zzQBDgkcFH_ff8B8C@jackal.rmq.cloudamqp.com/tqzvyxix') # URL professor
 params = pika.URLParameters(url)
+
 connection = pika.BlockingConnection(params)
 channel = connection.channel() # start a channel
-channel.queue_declare(queue='pdfprocess') # Declare a queue
+channel.queue_declare(queue='pdfprocess_aula') # Declare a queue
+
 # create a function which is called on incoming messages
 def callback(ch, method, properties, body):
- pdf_process_function(body)
+    pdf_process_function(body)
+
 # set up subscription on the queue
-channel.basic_consume('pdfprocess',
- callback,
- auto_ack=True)
+channel.basic_consume('pdfprocess', callback, auto_ack=True)
+
 # start consuming (blocks)
 channel.start_consuming()
 connection.close()
--- RabbitMQ/example_publisher.py
+++ RabbitMQ/example_publisher.py
@@ -1,14 +1,19 @@
 # example_publisher.py
 import pika, os, logging
+
 logging.basicConfig()
+
 # Parse CLODUAMQP_URL (fallback to localhost)
-url = os.environ.get('CLOUDAMQP_URL', 'amqps://yixrfvrp:Dqo7-CU2AmPrmkVhr1fqk2g6h7HMF2c5@jackal.rmq.cloudamqp.com/yixrfvrp')
+#url = os.environ.get('CLOUDAMQP_URL', 'amqps://yixrfvrp:Dqo7-CU2AmPrmkVhr1fqk2g6h7HMF2c5@jackal.rmq.cloudamqp.com/yixrfvrp')
+url = os.environ.get('CLOUDAMQP_URL','amqps://tqzvyxix:oGh4wcsnvBrG2U5zzQBDgkcFH_ff8B8C@jackal.rmq.cloudamqp.com/tqzvyxix') # URL professor
 params = pika.URLParameters(url)
 params.socket_timeout = 5
+
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
-channel.queue_declare(queue='pdfprocess')
+channel.queue_declare(queue='pdfprocess_aula')
+
 # send a message
-channel.basic_publish(exchange='', routing_key='pdfprocess', body='User information')
+channel.basic_publish(exchange='', routing_key='pdfprocess', body='Tá certo isso?!(Lucas)')
 print ("[x] Message sent to consumer")
 connection.close()
